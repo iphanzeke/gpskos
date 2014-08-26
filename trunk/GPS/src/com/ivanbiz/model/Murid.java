4f46310f9106b46095effd0b5d8cc8fe20da4f1b
iphanzeke <iphanzeke@134674f2-cb4b-8c93-2c74-ba1067beeb2f>
2014-08-26 14:38:46 +0000

    Master murid

---
+++ trunk/GPS/src/com/ivanbiz/model/Murid.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ivanbiz.model;
+
+/**
+ *
+ * @author ivan
+ */
+public class Murid {
+    private long id;
+    private String NIM;
+    private String nama;
+    private String email;
+    private String alamat;
+    private String telp;
+    private String handphone;
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the alamat
+     */
+    public String getAlamat() {
+        return alamat;
+    }
+
+    /**
+     * @param alamat the alamat to set
+     */
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    /**
+     * @return the telp
+     */
+    public String getTelp() {
+        return telp;
+    }
+
+    /**
+     * @param telp the telp to set
+     */
+    public void setTelp(String telp) {
+        this.telp = telp;
+    }
+
+    /**
+     * @return the handphone
+     */
+    public String getHandphone() {
+        return handphone;
+    }
+
+    /**
+     * @param handphone the handphone to set
+     */
+    public void setHandphone(String handphone) {
+        this.handphone = handphone;
+    }
+
+    /**
+     * @return the NIM
+     */
+    public String getNIM() {
+        return NIM;
+    }
+
+    /**
+     * @param NIM the NIM to set
+     */
+    public void setNIM(String NIM) {
+        this.NIM = NIM;
+    }
+    
+    
+}
