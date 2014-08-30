1ba0a1b75b8e68a5149712373b7fbdf3a3e68bda
iphanzeke <iphanzeke@134674f2-cb4b-8c93-2c74-ba1067beeb2f>
2014-08-30 09:30:23 +0000

    tambah fungsi

--- trunk/GPS/src/com/ivanbiz/model/Pembayaran.java
+++ trunk/GPS/src/com/ivanbiz/model/Pembayaran.java
@@ -23,4 +23,144 @@ public class Pembayaran {
     private Date datePosting;
     private String pathImage;
     private String transactionReference;
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the debitBankAccount
+     */
+    public BankAccount getDebitBankAccount() {
+        return debitBankAccount;
+    }
+
+    /**
+     * @param debitBankAccount the debitBankAccount to set
+     */
+    public void setDebitBankAccount(BankAccount debitBankAccount) {
+        this.debitBankAccount = debitBankAccount;
+    }
+
+    /**
+     * @return the kreditBankAccount
+     */
+    public BankAccount getKreditBankAccount() {
+        return kreditBankAccount;
+    }
+
+    /**
+     * @param kreditBankAccount the kreditBankAccount to set
+     */
+    public void setKreditBankAccount(BankAccount kreditBankAccount) {
+        this.kreditBankAccount = kreditBankAccount;
+    }
+
+    /**
+     * @return the jumlah
+     */
+    public double getJumlah() {
+        return jumlah;
+    }
+
+    /**
+     * @param jumlah the jumlah to set
+     */
+    public void setJumlah(double jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    /**
+     * @return the deskripsi
+     */
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    /**
+     * @param deskripsi the deskripsi to set
+     */
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the dateCreated
+     */
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    /**
+     * @param dateCreated the dateCreated to set
+     */
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    /**
+     * @return the datePosting
+     */
+    public Date getDatePosting() {
+        return datePosting;
+    }
+
+    /**
+     * @param datePosting the datePosting to set
+     */
+    public void setDatePosting(Date datePosting) {
+        this.datePosting = datePosting;
+    }
+
+    /**
+     * @return the pathImage
+     */
+    public String getPathImage() {
+        return pathImage;
+    }
+
+    /**
+     * @param pathImage the pathImage to set
+     */
+    public void setPathImage(String pathImage) {
+        this.pathImage = pathImage;
+    }
+
+    /**
+     * @return the transactionReference
+     */
+    public String getTransactionReference() {
+        return transactionReference;
+    }
+
+    /**
+     * @param transactionReference the transactionReference to set
+     */
+    public void setTransactionReference(String transactionReference) {
+        this.transactionReference = transactionReference;
+    }
 }
