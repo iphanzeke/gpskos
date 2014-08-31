36bdd71bc72eb865865c3b9cd69e78c1ed4d88f2
iphanzeke <iphanzeke@134674f2-cb4b-8c93-2c74-ba1067beeb2f>
2014-08-31 02:07:33 +0000

    tambah fungsi

---
+++ src/com/ivanbiz/model/Bank.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ivanbiz.model;
+
+/**
+ *
+ * @author ivan
+ */
+public class Bank {
+    private long id;
+    private String nama;
+    private String deskripsi;
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the deskripsi
+     */
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    /**
+     * @param deskripsi the deskripsi to set
+     */
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+}
