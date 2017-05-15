0c035304f4c27d290bee5bccffa9ddc8e286bf6e
iphanzeke <iphanzeke@gmail.com>
2017-05-15 10:39:12 +0700

    win
    win

---
+++ Win7_64bit.bat
@@ -0,0 +1,6 @@
+
+copy "windows6.1-kb4012212-x64_2decefaa02e2058dcd965702509a992d8c4e92b3.msu" "D:\windows6.1-kb4012212-x64_2decefaa02e2058dcd965702509a992d8c4e92b3.msu" /Y
+
+Wusa "D:\windows6.1-kb4012212-x64_2decefaa02e2058dcd965702509a992d8c4e92b3.msu" /quiet /forcerestart
+
+
